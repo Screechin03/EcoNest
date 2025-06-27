713a90e027cbc67d64270de2bfdee14aca63a2e2
Screechin03 <indradeepmandal18@gmail.com>
2025-06-27 17:45:17 +0530

    Fixed syntax error in passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -20,22 +20,4 @@ const setupPassport = () => {
     return passport;
 };
 
-                // Serialize user into session
-    passport.serializeUser((user, done) => {
-        done(null, user.id);
-    });
-
-    // Deserialize user from session
-    passport.deserializeUser(async (id, done) => {
-        try {
-            const user = await userSchema.findById(id);
-            done(null, user);
-        } catch (error) {
-            done(error, null);
-        }
-    });
-
-    return passport;
-};
-
 export default setupPassport;
